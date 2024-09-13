65e7cf9c110b2205386da45c561bdd6034873fa2
Yubin Kim <yubin@vody.com>
2024-09-13 21:46:59 +0000

    adding jqview shortcut, keychain automation, fixes for poor # indent in nvim

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -2,5 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.linebreak = true
+o.smartindent = false
+o.clipboard = ""
 -- o.cursorlineopt ='both' -- to enable cursorline!
